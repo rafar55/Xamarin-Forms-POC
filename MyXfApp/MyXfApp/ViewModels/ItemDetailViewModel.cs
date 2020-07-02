d3554b20c10f4a37fef7215703265b74ac9092b1
Rafael Romero <rafaromero4407@gmail.com>
2020-07-01 21:54:26 -0600

    Add project files.

---
+++ MyXfApp/MyXfApp/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MyXfApp.Models;
+
+namespace MyXfApp.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
